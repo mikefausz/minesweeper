40235ab7a0fd32b16f03e620b223f299040a0f34
Michael Fausz <mikefausz@Michaels-MacBook-Pro.local>
2016-05-07 22:04:28 -0400

    Created project files. Made basic minefield.

---
+++ minesweeper/controllers/home.controller.js
@@ -0,0 +1,24 @@
+angular
+  .module('minesweeper')
+  .controller('HomeController', function($scope) {
+    function createMinefield() {
+      var minefield = {};
+      minefield.rows = [];
+
+      for(var i = 0; i < 9; i++) {
+        var row = {};
+        row.spots = [];
+
+        for(var j = 0; j < 9; j++) {
+            var spot = {};
+            spot.isCovered = true;
+            row.spots.push(spot);
+        }
+
+        minefield.rows.push(row);
+      }
+
+      return minefield;
+    }
+    $scope.minefield = createMinefield();
+  });
